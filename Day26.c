347e52a4957b254561b0db63d0ea5ae2c4bd5013
saunitshaurya2006-commits <saunitshaurya2006@gmail.com>
2026-02-26 14:46:51 +0530

    Twenty-sixth Commit

---
+++ Day26.c
@@ -0,0 +1,78 @@
+// Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the linked list elements in forward order, space-separated
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
+// Explanation:
+// Each node has data, next, prev. Insert nodes sequentially, traverse from head using next pointer.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Doubly Linked List Node
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+// Function to insert node at the end
+struct Node* insert(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    // If list is empty
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+
+    // Traverse to last node
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+// Function to traverse and print list
+void traverse(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, x;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head = insert(head, x);
+    }
+
+    traverse(head);
+
+    return 0;
+}
