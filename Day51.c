81ed39125f597bdb56062fa0552e5f2e0063071d
saunitshaurya2006-commits <saunitshaurya2006@gmail.com>
2026-03-23 18:42:16 +0530

    Fifty-First Commit

---
+++ Day51.c
@@ -0,0 +1,86 @@
+// Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+// Input Format:
+// - First line contains integer N
+// - Second line contains N space-separated integers
+// - Third line contains two node values
+
+// Output Format:
+// - Print the LCA value
+
+// Example:
+// Input:
+// 7
+// 6 2 8 0 4 7 9
+// 2 8
+
+// Output:
+// 6
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for BST node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL)
+        return createNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+// Find LCA in BST
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root; // LCA found
+    }
+    return NULL;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    // Input BST values
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
