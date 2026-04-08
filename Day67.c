c2188de13dffe22562f666442865d76b4cc29e9f
saunitshaurya2006-commits <saunitshaurya2006@gmail.com>
2026-04-08 10:36:28 +0530

    Sixty-seventh Commit

---
+++ Day67.c
@@ -0,0 +1,72 @@
+// Print topological ordering of a Directed Acyclic Graph (DAG) using DFS.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int stack[MAX];
+int top = -1;
+int n;
+
+// Push into stack
+void push(int v) {
+    stack[++top] = v;
+}
+
+// DFS function
+void dfs(int v) {
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+
+    push(v); // push after visiting all neighbors
+}
+
+// Topological Sort
+void topoSort() {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    printf("Topological Order:\n");
+    while (top != -1) {
+        printf("%d ", stack[top--]);
+    }
+}
+
+int main() {
+    int e, u, v;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    // Initialize adjacency matrix
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    printf("Enter number of edges: ");
+    scanf("%d", &e);
+
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < e; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1; // directed edge
+    }
+
+    topoSort();
+
+    return 0;
+}
