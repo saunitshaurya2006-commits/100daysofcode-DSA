6c7d39c9a4ebd53f3d8d0bbdc93cf70ff47ced12
saunitshaurya2006-commits <saunitshaurya2006@gmail.com>
2026-04-11 23:36:37 +0530

    Seventieth Commit

---
+++ Day70.c
@@ -0,0 +1,73 @@
+// Compute shortest path from source and detect negative weight cycles using Bellman-Ford.
+
+// Input:
+// - n vertices
+// - m edges (u,v,w)
+
+// Output:
+// - Shortest distances OR NEGATIVE CYCLE
+
+#include <stdio.h>
+#include <limits.h>
+
+typedef struct {
+    int u, v, w;
+} Edge;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    Edge edges[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    }
+
+    int src;
+    scanf("%d", &src);
+
+    int dist[n];
+
+    // Initialize distances
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX;
+
+    dist[src] = 0;
+
+    // Relax edges n-1 times
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    // Check negative cycle
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+
+        if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE\n");
+            return 0;
+        }
+    }
+
+    // Output distances
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == INT_MAX)
+            printf("INF ");
+        else
+            printf("%d ", dist[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
